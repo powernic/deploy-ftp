a22562d539ce895f2fd070bd2bd0fe64d0d7b42c
Nikolay Efimenko <powernic@list.ru>
2017-04-26 14:33:15 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
 gitLastCommit=$(git show --summary --grep="Merge pull request")
 if [[ -z "$gitLastCommit" ]]
 then
@@ -12,7 +13,9 @@ else
 fi
 echo $lastCommit
 
-filesChanged=$(git diff-tree --no-commit-id --name-only -r $lastCommit)
+filesChanged=$(git diff-tree --no-commit-id --name-only --diff-filter=d -r $lastCommit)
+filesRemoved=$(git diff-tree --no-commit-id --name-only --diff-filter=D -r $lastCommit)
+
 if [ ${#filesChanged[@]} -eq 0 ]; then
     echo "No files to update"
 else
@@ -22,7 +25,20 @@ else
 		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
 		then
 	 		echo "Uploading $f"
-	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://bzamok.askerweb.ru/$f
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.askerweb.by/public_html/$f
+		fi
+	done
+fi
+if [ ${#filesRemoved[@]} -eq 0 ]; then
+    echo "No files to remove"
+else
+    for f in $filesRemoved
+	do
+		#do not upload these files that aren't necessary to the site
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
+		then
+	 		echo "Removed $f"
+	 		curl -v -u $FTP_USER:$FTP_PASS ftp://ftp.askerweb.by/public_html/ -Q "DELE public_html/$f" > /dev/null
 		fi
 	done
 fi
